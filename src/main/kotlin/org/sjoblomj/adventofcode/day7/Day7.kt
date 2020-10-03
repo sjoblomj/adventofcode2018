7546a10585525f68d5cdcb87f675b22efb6e7ca1
Johan Sjöblom <sjoblomj@student.chalmers.se>
2020-10-03 11:44:23 +0200

    Day 7 part 2

--- src/main/kotlin/org/sjoblomj/adventofcode/day7/Day7.kt
+++ src/main/kotlin/org/sjoblomj/adventofcode/day7/Day7.kt
@@ -7,6 +7,8 @@ import org.sjoblomj.adventofcode.readFile
 import kotlin.system.measureTimeMillis
 
 private const val inputFile = "src/main/resources/inputs/day7.txt"
+private const val numberOfWorkers = 5
+private const val baseTimeTakenForEachStep = 60
 
 
 fun day7() {
@@ -18,9 +20,12 @@ fun day7() {
 private fun calculateAndPrintDay7() {
   val nodes = createNodes(readFile(inputFile))
   println("Resulting order is ${orderNodes(nodes)}")
+
+  val parallelizedWork = parallelize(nodes, numberOfWorkers, baseTimeTakenForEachStep)
+  println("With parallelization, it would take ${parallelizedWork.size - 1} seconds to finish")
 }
 
-fun createNodes(input: List<String>): List<Node> {
+internal fun createNodes(input: List<String>): List<Node> {
   return createNodesFromInput(input)
 }
 
@@ -42,10 +47,10 @@ private fun orderNodesRecursively(node: Node, orderedNodes: MutableList<Node>) {
   }
 }
 
-private fun allDependenciesFulfilled(node: Node, orderedNodes: List<Node>) =
-  orderedNodes.containsAll(node.prerequisites)
+private fun allDependenciesFulfilled(node: Node, finishedNodes: List<Node>) =
+  finishedNodes.containsAll(node.prerequisites)
 
-fun getFirstNodes(nodes: List<Node>): List<Node> {
+private fun getFirstNodes(nodes: List<Node>): List<Node> {
   val firstNodes = nodes.filter { it.prerequisites.isEmpty() }.sortedBy { it.id }
 
   if (firstNodes.isEmpty())
@@ -54,6 +59,73 @@ fun getFirstNodes(nodes: List<Node>): List<Node> {
 }
 
 
-fun output(nodes: List<Node>, resultingOrder: String) {
+internal fun parallelize(nodes: List<Node>, numberOfWorkers: Int, baseTimeTakenForEachStep: Int): List<WorkOrder> {
+  val workers = generateAvailableWorkers(numberOfWorkers, baseTimeTakenForEachStep)
+
+  return parallelize(nodes.sortedBy { it.id }, 0, workers, emptyList()).reversed()
+}
+
+private fun parallelize(nodes: List<Node>, iteration: Int, workers: List<Worker>, result: List<Node>): List<WorkOrder> {
+
+  val nodesThatWereJustFinished = workers.filter { it.isFinished(iteration) }.mapNotNull { it.node }
+  val finishedNodes = result.plus(nodesThatWereJustFinished)
+
+  if (nodes.isEmpty() && workers.all { it.isFinished(iteration) })
+    return listOf(WorkOrder(generateAvailableWorkers(workers.size, workers.first().baseTimeTakenForEachStep), finishedNodes))
+
+  val newWorkerList = createNewWorkerList(nodes, finishedNodes, workers, iteration)
+
+  return parallelize(getNodesNotBeingWorkedOn(newWorkerList, nodes), iteration + 1, newWorkerList, finishedNodes).plus(WorkOrder(newWorkerList, finishedNodes))
+}
+
+private fun createNewWorkerList(nodes: List<Node>, finishedNodes: List<Node>, workers: List<Worker>, iteration: Int): List<Worker> {
+
+  val nodesReadyForProcessing = nodes.filter { allDependenciesFulfilled(it, finishedNodes) }
+  var nodesReadyForProcessingIndex = 0
+
+  return workers.map {
+    if (it.isFinished(iteration)) {
+
+      val node = if (nodesReadyForProcessingIndex < nodesReadyForProcessing.size) nodesReadyForProcessing[nodesReadyForProcessingIndex] else null
+      nodesReadyForProcessingIndex++
+      Worker(node, iteration, it.baseTimeTakenForEachStep)
+    } else
+      it
+  }
+}
+
+private fun getNodesNotBeingWorkedOn(newWorkerList: List<Worker>, nodes: List<Node>): List<Node> {
+  val nodesBeingWorkedOn = newWorkerList.mapNotNull { it.node }
+  return nodes.filter { !nodesBeingWorkedOn.contains(it) }
+}
+
+private fun generateAvailableWorkers(numberOfWorkers: Int, baseTimeTakenForEachStep: Int) =
+  generateSequence { Worker(null, null, baseTimeTakenForEachStep) }.take(numberOfWorkers).toList()
+
+
+internal data class WorkOrder(val workers: List<Worker>, val result: List<Node>)
+
+internal data class Worker(val node: Node?, val startedAt: Int?, val baseTimeTakenForEachStep: Int) {
+  private fun delayForJob() = if (node == null) -1 else node.id[0].toInt() - 'A'.toInt() + 1
+  fun isFinished(currentIteration: Int) = if (startedAt == null || node == null) true else startedAt + baseTimeTakenForEachStep + delayForJob() <= currentIteration
+}
+
+
+internal fun visualiseParallelizedWork(workOrders: List<WorkOrder>): String {
+  val numberOfWorkers = workOrders.map { it.workers.size }.max()
+  val workerLabels = (0 until (numberOfWorkers ?: 0)).joinToString(" ")
+  val secondLabel = "Second"
+  val label = "$secondLabel $workerLabels    Done"
+
+  val lines = workOrders.mapIndexed { index, workOrder ->
+    val formattedIndex = index.toString().padEnd(secondLabel.length)
+    val underWork = workOrder.workers.map { it.node?.id ?: "." }
+
+    "$formattedIndex ${underWork.joinToString(" ")}    ${workOrder.result.joinToString("") { it.id }}"
+  }
+  return label + "\n" + lines.joinToString("\n")
+}
+
+private fun output(nodes: List<Node>, resultingOrder: String) {
   GlobalScope.launch { visualise(nodes, resultingOrder) }
 }
--- src/test/kotlin/org/sjoblomj/adventofcode/day7/Day7Tests.kt
+++ src/test/kotlin/org/sjoblomj/adventofcode/day7/Day7Tests.kt
@@ -34,4 +34,43 @@ class Day7Tests {
 
     assertEquals("CABDFE", result)
   }
+
+
+  @Test fun `Can parallelize work`() {
+    val input = listOf(
+      "Step C must be finished before step A can begin.",
+      "Step C must be finished before step F can begin.",
+      "Step A must be finished before step B can begin.",
+      "Step A must be finished before step D can begin.",
+      "Step B must be finished before step E can begin.",
+      "Step F must be finished before step E can begin.",
+      "Step D must be finished before step E can begin."
+    )
+    val nodes = createNodes(input)
+
+    val numberOfWorkers = 2
+    val baseTimeTakenForEachStep = 0
+
+    val expected = "" +
+      "Second 0 1    Done\n" +
+      "0      C .    \n" +
+      "1      C .    \n" +
+      "2      C .    \n" +
+      "3      A F    C\n" +
+      "4      B F    CA\n" +
+      "5      B F    CA\n" +
+      "6      D F    CAB\n" +
+      "7      D F    CAB\n" +
+      "8      D F    CAB\n" +
+      "9      D .    CABF\n" +
+      "10     E .    CABFD\n" +
+      "11     E .    CABFD\n" +
+      "12     E .    CABFD\n" +
+      "13     E .    CABFD\n" +
+      "14     E .    CABFD\n" +
+      "15     . .    CABFDE"
+
+    val result = parallelize(nodes, numberOfWorkers, baseTimeTakenForEachStep)
+    assertEquals(expected, visualiseParallelizedWork(result))
+  }
 }
